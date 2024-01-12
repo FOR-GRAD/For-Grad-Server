8f2b3757e9ab416c607ce7eb41b1a698dd0f452e
JunHyun <junhyun1001@gmail.com>
2024-01-12 18:44:54 +0900

    init

---
+++ src/main/java/umc/forgrad/ForGradApplication.java
@@ -0,0 +1,13 @@
+package umc.forgrad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForGradApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForGradApplication.class, args);
+    }
+
+}
---
+++ src/test/java/umc/forgrad/ForGradApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.forgrad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForGradApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
