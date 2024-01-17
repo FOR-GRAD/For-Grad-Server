c771aa43c8014c0ba6786b1f392048fada2c920f
ezeun <consistency1231@gmail.com>
2024-01-17 10:58:50 +0900

    이슈 #39 FreeService 생성

---
+++ src/main/java/umc/forgrad/service/FreeService.java
@@ -0,0 +1,40 @@
+package umc.forgrad.service;
+
+import umc.forgrad.domain.common.Free;
+import umc.forgrad.repository.FreeRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public class FreeService{
+
+    private final FreeRreository freeRepository;
+
+    public FreeService(FreeRepository freeRepository){
+        this.freeRepository = freeRepository;
+    }
+
+    /**
+     * 메모 추가하기
+     */
+    public Long addMemo(Free free){
+        freeRepository.save(free);
+        return free.getId();
+    }
+
+    /**
+     * 메모 조회하기
+     */
+    public List<Free> findMemos(){
+        return freeRepository.findAll();
+    }
+
+    public Optional<Free> findOne(Long memoId){
+        return freeRepository.findById(memoId);
+    }
+}
