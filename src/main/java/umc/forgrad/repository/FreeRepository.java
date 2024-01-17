9085924161ed691e60b03f92db385c1abc49190e
ezeun <consistency1231@gmail.com>
2024-01-17 10:28:27 +0900

    이슈 #39 FreeRepository 생성

---
+++ src/main/java/umc/forgrad/repository/FreeRepository.java
@@ -0,0 +1,12 @@
+package umc.forgrad.repository;
+
+import umc.forgrad.domain.common.Free;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FreeRepository {
+    Free save(Free free);
+    Optional<Free> findById(Long id);
+    List<Free> findAll();
+}
