da4e1860f1cde423540493751ce3d011a5a4c7bd
김종현 <sumei88@naver.com>
2024-01-22 18:57:25 +0900

    컨트롤러의 파라미터 밸류 수정

--- src/main/java/umc/forgrad/controller/ActivityController.java
+++ src/main/java/umc/forgrad/controller/ActivityController.java
@@ -21,12 +21,11 @@ public class ActivityController {
 
     @PostMapping("/activity")
     public ApiResponse<PostActivityResponse.RegistActivityResultDto> createActivity(@RequestPart(value = "image", required = false)List<MultipartFile> multipartFiles,
-                                                                                    @RequestPart(value = "file", required = false)PostActivityRequest.RegistActivity registActivity) {
+                                                                                    @RequestPart(value = "requestDto", required = false)PostActivityRequest.RegistActivity registActivity) {
 
-//            PostActivityRequest postActivityRequest = new PostActivityRequest(title, content); dto를 여기서 만들었던건데, 스터디에서의 방식은 dto를 컨트롤러 인자로 받아서 서비스에는 dto를 그대로 넣어버림.
-//            그럼 서비스에는 컨버터 이용하는ㄴ 부분이 있어서 dto를 엔티티로 잘 변환해서 씀
+
         Activity activity = activityCommandService.createBoard(registActivity, multipartFiles);
-//            return ApiResponse.onSuccess(activityCommandService.createBoard(postActivityRequest, multipartFiles));
+
         return ApiResponse.onSuccess(ActivityConverter.activityResultDto(activity));
 
     }
