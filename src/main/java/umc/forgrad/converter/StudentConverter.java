be8fdea7847614a5b25c5b9bca38d54e3f20650d
JunHyun <junhyun1001@gmail.com>
2024-01-22 22:55:49 +0900

    Student Entity 저장

--- src/main/java/umc/forgrad/converter/StudentConverter.java
+++ src/main/java/umc/forgrad/converter/StudentConverter.java
@@ -1,8 +1,15 @@
 package umc.forgrad.converter;
 
+import umc.forgrad.domain.Student;
 import umc.forgrad.dto.student.StudentResponseDto;
 
 public class StudentConverter {
+
+    public static Student toStudent(Long id) {
+        return Student.builder()
+                .id(id)
+                .build();
+    }
 
     public static StudentResponseDto.LoginResponseDto toLoginResultDto(String loginResultMessage) {
         return StudentResponseDto.LoginResponseDto.builder()
