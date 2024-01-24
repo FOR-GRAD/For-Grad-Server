9e547a4cda3e76174ca80731c904dc7060eb8eab
이현주 <lhjivy1024@gmail.com>
2024-01-24 16:37:04 +0900

    #42 [Feat] 자격증 추가, 조회 테스트까지 완료

---
+++ src/main/java/umc/forgrad/dto/ViewCertificateResponseDto.java
@@ -0,0 +1,23 @@
+package umc.forgrad.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.BindParam;
+import umc.forgrad.domain.Certificate;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+public class ViewCertificateResponseDto {
+    private String name;
+    private LocalDate date;
+
+    @Builder
+    public ViewCertificateResponseDto(String name, LocalDate date) {
+        this.name = name;
+        this.date = date;
+    }
+}
