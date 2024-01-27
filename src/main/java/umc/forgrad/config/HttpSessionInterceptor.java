a761e8f27ef30b0493daa30dab7cfadc5b692216
JunHyun <junhyun1001@gmail.com>
2024-01-27 23:31:38 +0900

    미사용 override 메소드 제거

--- src/main/java/umc/forgrad/config/HttpSessionInterceptor.java
+++ src/main/java/umc/forgrad/config/HttpSessionInterceptor.java
@@ -27,13 +27,4 @@ public class HttpSessionInterceptor implements HandlerInterceptor {
 
     }
 
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        log.info("postHandle");
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        log.info("afterCompletion");
-    }
 }
