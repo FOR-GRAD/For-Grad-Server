efaa296fec793852a0ed71af12ac03c61a9a777c
JunHyun <junhyun1001@gmail.com>
2024-01-27 23:48:50 +0900

    import 제거

--- src/main/java/umc/forgrad/config/HttpSessionInterceptor.java
+++ src/main/java/umc/forgrad/config/HttpSessionInterceptor.java
@@ -3,14 +3,12 @@ package umc.forgrad.config;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import umc.forgrad.apipayload.code.status.ErrorStatus;
 import umc.forgrad.exception.GeneralException;
 
 @Component
-@Slf4j
 public class HttpSessionInterceptor implements HandlerInterceptor {
 
     @Override
