da2ce7edb1e5b10421cf8c0ca212343d1728c6ef
JunHyun <junhyun1001@gmail.com>
2024-01-31 22:58:55 +0900

    Subject, Semester Entity 변경

--- src/main/java/umc/forgrad/domain/Semester.java
+++ src/main/java/umc/forgrad/domain/Semester.java
@@ -2,9 +2,7 @@ package umc.forgrad.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
-import umc.forgrad.domain.Student;
 import umc.forgrad.domain.common.BaseEntity;
-import umc.forgrad.domain.mapping.SemesterSubject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +13,7 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Semester extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,11 +24,11 @@ public class Semester extends BaseEntity {
     @Column(nullable = false)
     private Integer semester;
 
+    @OneToMany(mappedBy = "semester", cascade = CascadeType.ALL)
+    private List<Subject> subjectList = new ArrayList<>();
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "student_id")
     private Student student; //Student 엔티티와 연결
-
-    @OneToMany(mappedBy = "semester")
-    private List<SemesterSubject> semesterSubjectList = new ArrayList<>();
 
 }
