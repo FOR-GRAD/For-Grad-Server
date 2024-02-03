36488579865c207c4c5c659fa183e9088cd55781
JunHyun <junhyun1001@gmail.com>
2024-02-03 15:47:40 +0900

    entity 수정한거 적용

--- src/main/java/umc/forgrad/repository/SubjectRepository.java
+++ src/main/java/umc/forgrad/repository/SubjectRepository.java
@@ -3,15 +3,15 @@ package umc.forgrad.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import umc.forgrad.domain.Semester;
 import umc.forgrad.domain.Subject;
+import umc.forgrad.domain.Timetable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
 
-    List<Subject> findBySemester(Semester semester);
+    List<Subject> findByTimetable(Timetable timetable);
 
     @Query("SELECT SUM(s.credit) FROM Subject s WHERE s IN :subjects")
     Optional<Integer> sumCredits(@Param("subjects") List<Subject> subjectList);
