5d8d36c5bc52c21dceff5de17eb58f6adda652bb
김종현 <sumei88@naver.com>
2024-02-05 21:44:14 +0900

    s3관련오류, 원인(로깅 fileter,interceptor), 해결완료

--- src/main/java/umc/forgrad/filter/CustomServletWrappingFilter.java
+++ src/main/java/umc/forgrad/filter/CustomServletWrappingFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
@@ -16,12 +17,18 @@ public class CustomServletWrappingFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        if (request instanceof StandardMultipartHttpServletRequest) {
+            // multipart 요청의 경우, 캐시를 사용하지 않고 원래의 요청을 그대로 사용
+            chain.doFilter(request, response);
+        } else {
+            // 그 외의 요청의 경우, 캐시를 사용
+            ContentCachingRequestWrapper wrappingRequest = new ContentCachingRequestWrapper(request);
+            ContentCachingResponseWrapper wrappingResponse = new ContentCachingResponseWrapper(response);
 
-        ContentCachingRequestWrapper wrappingRequest = new ContentCachingRequestWrapper(request);
-        ContentCachingResponseWrapper wrappingResponse = new ContentCachingResponseWrapper(response);
+            chain.doFilter(wrappingRequest, wrappingResponse);
 
-        chain.doFilter(wrappingRequest, wrappingResponse);
-
-        wrappingResponse.copyBodyToResponse();
+            wrappingResponse.copyBodyToResponse();
+        }
     }
+
 }
